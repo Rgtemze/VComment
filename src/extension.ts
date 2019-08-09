449c078ef3dc2e496c56476750ad76bdb928cd35
Ziya Erkoc <ziya.erkoc@ug.bilkent.edu.tr>
2019-08-09 22:31:59 +0300

    Function scope considered while assigning comments - Block scope is not considered yet.

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,21 @@
 import * as vscode from 'vscode';
 
 
-const comments : {[docId: string]: {[variable: string]: string}} = {};
+
+class Node{
+	symbol: vscode.DocumentSymbol;
+	comment: string;
+	kind: vscode.SymbolKind;
+	children: Node[]
+	constructor(symbol: vscode.DocumentSymbol, kind: vscode.SymbolKind, comment: string){
+		this.symbol = symbol;
+		this.comment = comment;
+		this.kind = kind;
+		this.children = [];
+	}
+}
+
+let allSymbols: {[documentId: string] :Node[]} = {};
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -16,9 +30,14 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	vscode.workspace.onDidSaveTextDocument((doc: vscode.TextDocument) => {
+	vscode.workspace.onDidSaveTextDocument(_ => {
 		calculateVariables();
 	});
+	
+	vscode.workspace.onDidOpenTextDocument(_ => {
+		calculateVariables();
+	});
+
 	let disposable = vscode.commands.registerCommand('extension.vcomment', () => {
 		// The code you place here will be executed every time your command is executed
 		calculateVariables();
@@ -46,16 +65,19 @@ export function activate(context: vscode.ExtensionContext) {
 			let match;
 			let prev = -1;
 			let variable = "";
+			let range = new vscode.Range(_position, _position);
 			while (match = patt.exec(line)) {
 				if(match.index > charPos){
 					variable = line.substr(prev + 1, match.index - prev - 1);
+					range = new vscode.Range(new vscode.Position(_position.line, prev + 1) , new vscode.Position(_position.line, match.index - 1));
 					break;
 				}
 				prev = match.index;
 			}
 
-			const comment = comments[_document.uri.fsPath][variable];
+			const comment = findSymbol(allSymbols[_document.uri.fsPath], {range: range, name: variable}, null);
 			if(variable && comment){
+				console.log("findSymbol");
 				contents = new vscode.MarkdownString(`${comment}`);
 			}
 	
@@ -65,44 +87,96 @@ export function activate(context: vscode.ExtensionContext) {
 	  );
 }
 
+function rebuildSymbols(symbols: vscode.DocumentSymbol[]): Node[]{
+	if(!symbols || symbols.length == 0){
+		return [];
+	}
+
+	return symbols.map(symbol => {
+		if(symbol.kind === vscode.SymbolKind.Variable){
+			const activeEditor = vscode.window.activeTextEditor;
+
+			if(symbol.range.start.line == 0){
+				return new Node(symbol, symbol.kind, "");
+			} else{
+				const sel = new vscode.Selection(symbol.range.start.line - 1, 0, symbol.range.start.line, 0);
+				let text = activeEditor!.document.getText(sel);
+				
+				const index = text.indexOf("\n");
+				if(index != -1)
+					text = text.substr(0, index);
+				const atPos = text.indexOf("@");
+				if(atPos != -1){
+					return new Node(symbol, symbol.kind, text.substr(atPos + 1));
+				}
+			}
+			return new Node(symbol, symbol.kind, "");
+		} else {
+			const newSymbol = new Node(symbol, symbol.kind, "B");
+			newSymbol.children = rebuildSymbols(symbol.children);
+			return newSymbol;
+		}
+	});
+}
+
+
+function findSymbol(nodes: Node[], target: {range: vscode.Range, name: string}, parentRange: null | vscode.Range): string | null{
+	if(!target){
+		return null;
+	}
+
+	if(!nodes || nodes.length == 0){
+		return null;
+	}
+
+	for(const node of nodes){
+
+		if(node.kind !== vscode.SymbolKind.Function){
+			continue;
+		}
+		
+		if(!parentRange || parentRange.contains(target.range)){
+			let comment = findSymbol(node.children, target, node.symbol.range);
+			if(comment){
+				return comment;
+			}
+		}
+	}
+
+	for(const node of nodes){
+		if(node.kind !== vscode.SymbolKind.Variable){
+			continue;
+		}
+		if((!parentRange || parentRange.contains(target.range)) && node.symbol.name === target.name){
+			return node.comment;
+		}
+	}
+
+	return null;
+}
+
 // this method is called when your extension is deactivated
 export function deactivate() {}
 
 // Below function is acquired from https://stackoverflow.com/questions/57345173/getting-locations-of-the-variable-declarations
 
 function findVars(symbols: vscode.DocumentSymbol[]): vscode.DocumentSymbol[] {
-	console.log(symbols);
+	//console.log(symbols);
 	var vars =
 		symbols.filter(symbol => symbol.kind === vscode.SymbolKind.Variable);
 	return vars.concat(symbols.map(symbol => findVars(symbol.children))
-							.reduce((a, b) => a.concat(b), []));
+								.reduce((a, b) => a.concat(b), []));
 }
 
 function calculateVariables(){
-	var activeEditor = vscode.window.activeTextEditor;
+	const activeEditor = vscode.window.activeTextEditor;
 	if (activeEditor !== undefined) {
-	comments[activeEditor.document.uri.fsPath] = {};
 	vscode.commands
 		.executeCommand<vscode.DocumentSymbol[]>(
 			'vscode.executeDocumentSymbolProvider', activeEditor.document.uri)
 		.then(symbols => {
 			if (symbols !== undefined) {
-				for (const variable of findVars(symbols)) {
-					if(variable.range.start.line == 0){
-						continue;
-					}
-					const sel = new vscode.Selection(variable.range.start.line - 1, 0, variable.range.start.line, 0);
-					let text = activeEditor!.document.getText(sel);
-					
-					const index = text.indexOf("\n");
-					if(index != -1)
-						text = text.substr(0, index);
-					const atPos = text.indexOf("@");
-					if(atPos == -1){
-						continue;
-					}
-					comments[activeEditor!.document.uri.fsPath][variable.name] = text.substr(atPos + 1);
-			}
+				allSymbols[activeEditor.document.uri.fsPath] = rebuildSymbols(symbols);
 			}
 		});
 	}
